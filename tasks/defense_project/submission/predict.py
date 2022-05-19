216538229cfbe4b3b0e373b0386e97f742eda203
Justin Yue <justinyue1643@gmail.com>
2022-05-19 10:33:10 -0700

    remove transforms

--- tasks/defense_project/submission/predict.py
+++ tasks/defense_project/submission/predict.py
@@ -55,8 +55,6 @@ class Prediction():
 
     def preprocess(self, original_images):
         perturbed_image = original_images.unsqueeze(0)
-        blurrer = T.GaussianBlur(kernel_size=4)
-        perturbed_image = blurrer(perturbed_image)
         return perturbed_image
 
     def detect_attack(self, original_image):
